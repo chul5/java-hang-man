8c4590bc8f6ff5d7df697a44406c2ce7246fed5e
cheol5 <och7164@gmail.com>
2024-02-05 02:02:40 +0900

    Feat: 단어입력을 검증하는 기능

--- src/main/java/hangman/domain/Answer.java
+++ src/main/java/hangman/domain/Answer.java
@@ -1,9 +1,20 @@
 package hangman.domain;
 
+import java.util.List;
+
 public class Answer {
     private String answer;
 
     public Answer(String input) {
         answer = input;
+    }
+
+    public void validateInputLength(String input) {
+        if (answer.length() != input.length())
+            throw new IllegalArgumentException();
+    }
+
+    public boolean isContained(String input) {
+        return answer.contains(input);
     }
 }
--- src/main/java/hangman/service/Validator.java
+++ src/main/java/hangman/service/Validator.java
@@ -1,11 +1,23 @@
 package hangman.service;
 
 public class Validator {
-    public void alpha(String input) {
+    private final int ALPHA = 1;
+
+    public void input(String input) {
         if (input.isEmpty())
             throw new IllegalArgumentException();
+        if (input.length() == ALPHA)
+            alpha(input);
+        if (input.length() > ALPHA)
+            word(input);
+    }
+    public void alpha(String input) {
         if (!input.chars()
                 .allMatch(i -> 'a' <= i && i <= 'z'))
             throw new IllegalArgumentException();
+    }
+
+    public void word(String input) {
+
     }
 }
