08996bd980272756b6b3f82aa3012627e10d2677
cheol5 <och7164@gmail.com>
2024-02-05 01:38:34 +0900

    Feat: 입력값이 '공백'과 '소문자'인지 검증하는 기능

--- src/main/java/hangman/service/Validator.java
+++ src/main/java/hangman/service/Validator.java
@@ -1,2 +1,12 @@
-package hangman.service;public class Validator {
+package hangman.service;
+
+public class Validator {
+    public void word(String input) {
+        if (input.isEmpty())
+            throw new IllegalArgumentException();
+        if (!input.chars()
+                .allMatch(i -> 'a' <= i && i <= 'z'))
+            throw new IllegalArgumentException();
+
+    }
 }
