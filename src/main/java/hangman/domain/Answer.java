49a0180d5bbfe95c9bae0311d87cda84178dcff5
cheol5 <och7164@gmail.com>
2024-02-04 22:05:01 +0900

    Feat: 파일에서 단어를 불러오는 기능

---
+++ src/main/java/hangman/domain/Answer.java
@@ -0,0 +1,9 @@
+package hangman.domain;
+
+public class Answer {
+    private String answer;
+
+    public Answer(String input) {
+        answer = input;
+    }
+}
---
+++ src/main/java/hangman/service/GameService.java
@@ -0,0 +1,26 @@
+package hangman.service;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GameService {
+    private final String FILE_PATH = "src/resources/words.md";
+
+
+    public List<String> getWords() {
+        List<String> words = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                words.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return words;
+    }
+}
