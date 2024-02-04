573f6b0241a99eb0dc1700256c293299fbec9e59
cheol5 <och7164@gmail.com>
2024-02-05 01:42:16 +0900

    Refactor: 검증 기능의 이름 수정

--- src/main/java/hangman/service/Validator.java
+++ src/main/java/hangman/service/Validator.java
@@ -1,12 +1,11 @@
 package hangman.service;
 
 public class Validator {
-    public void word(String input) {
+    public void alpha(String input) {
         if (input.isEmpty())
             throw new IllegalArgumentException();
         if (!input.chars()
                 .allMatch(i -> 'a' <= i && i <= 'z'))
             throw new IllegalArgumentException();
-
     }
 }
