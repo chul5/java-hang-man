9a60d9caa26a7333a185a649fe5985958a195a15
cheol5 <och7164@gmail.com>
2024-02-05 09:20:17 +0900

    Refactor: 정적 프로퍼티 수정

--- src/main/java/hangman/view/InputView.java
+++ src/main/java/hangman/view/InputView.java
@@ -5,9 +5,9 @@ import hangman.service.Validator;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner SC = new Scanner(System.in);
+    private final Scanner SC = new Scanner(System.in);
 
-    public static String getConjecture() {
+    public String getConjecture() {
         System.out.println("유추한 알파벳 또는 단어를 입력해 주세요.");
         return Validator.input(SC.nextLine().trim());
     }
