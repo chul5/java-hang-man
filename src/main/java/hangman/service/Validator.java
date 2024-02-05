a2e291a3ba3f150fe2f1fa1d426afc7500e6740b
cheol5 <och7164@gmail.com>
2024-02-05 09:01:46 +0900

    Refactor: 입력값 검증 메서드 수정

--- src/main/java/hangman/service/Validator.java
+++ src/main/java/hangman/service/Validator.java
@@ -3,21 +3,16 @@ package hangman.service;
 public class Validator {
     private final int ALPHA = 1;
 
-    public void input(String input) {
+    public String input(String input) {
         if (input.isEmpty())
             throw new IllegalArgumentException();
         if (input.length() == ALPHA)
             alpha(input);
-        if (input.length() > ALPHA)
-            word(input);
+        return input;
     }
     public void alpha(String input) {
         if (!input.chars()
                 .allMatch(i -> 'a' <= i && i <= 'z'))
             throw new IllegalArgumentException();
-    }
-
-    public void word(String input) {
-
     }
 }
