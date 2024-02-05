f37d20da2987f83cf3b58d2426789f9aea92ca42
cheol5 <och7164@gmail.com>
2024-02-05 09:30:33 +0900

    Docs: 기능 목록 수정

---
+++ src/main/java/hangman/service/InputHandler.java
@@ -0,0 +1,15 @@
+package hangman.service;
+
+import java.util.function.Supplier;
+
+public class InputHandler {
+    public static <T> T handle(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
